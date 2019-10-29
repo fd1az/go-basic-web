2311dd0cecaab859c24e6c38b1db49f4d10477c9
Fdiaz7 <facu.diaz777@gmail.com>
2019-10-29 07:39:05 -0300

    adding render method

--- views/views.go
+++ views/views.go
@@ -2,6 +2,7 @@ package views
 
 import (
 	"html/template"
+	"net/http"
 	"path/filepath"
 )
 
@@ -36,4 +37,9 @@ func loyoutFiles() []string {
 		panic(err)
 	}
 	return files
+}
+
+//Render is used to render the view with the predefined layout
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
 }
