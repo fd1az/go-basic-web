6e1fdf1dfb8e421fb41fb1580820042927c2e22b
Fdiaz7 <facu.diaz777@gmail.com>
2019-10-29 23:46:57 -0300

    signup

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 var (
 	homeView    *views.View
 	contactView *views.View
+	signupView  *View.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -28,10 +29,18 @@ func contact(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 }
-
+func sigup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	err := signupView.Template.ExecuteTemplate(w, signupView.Layout, nil)
+	if err != nil {
+		panic(err)
+	}
+}
 func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
