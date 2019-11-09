b621ebdcda5703e40738822c72583bf9266b34ce
Fdiaz7 <facu.diaz777@gmail.com>
2019-11-09 10:00:22 -0300

    user controller

---
+++ controllers/users.go
@@ -0,0 +1,41 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/fdiaz7/go-basic-web/views"
+)
+
+//NewUsers is used to create a new Users controllers
+//This function will panic if the templates are not parse correctly
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+//Users struct
+type Users struct {
+	NewView *views.View
+}
+
+//SignupForm struc
+type SignupForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
+//New GET -> This is user tu render the form where a user can create a new user acount
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	u.NewView.Render(w, nil)
+}
+
+//Create POST message
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	var form SignupForm
+	if err := parseForm(r, &form); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
+}
